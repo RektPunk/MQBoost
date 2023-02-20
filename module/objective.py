6585b97420dc8b462079901b8870e168fe364ed1
RektPunk <rektpunk@gmail.com>
2023-02-20 23:56:18 +0900

    feat - add docstring in objective

--- module/objective.py
+++ module/objective.py
@@ -14,6 +14,18 @@ def _grad_rho(u, alpha) -> np.ndarray:
 def check_loss_grad_hess(
     y_pred: np.ndarray, dtrain: lgb.basic.Dataset, alphas: List[float]
 ) -> Tuple[np.ndarray, np.ndarray]:
+    """
+    Return gradient and hessin of composite quanitle loss
+    Args:
+        y_pred (np.ndarray)
+        dtrain (lgb.basic.Dataset)
+        alphas (List[float])
+
+    Returns:
+        Tuple[np.ndarray, np.ndarray]:
+            gradient
+            hessian
+    """
     _len_alpha = len(alphas)
     _y_train = dtrain.get_label()
     _y_pred_reshaped = y_pred.reshape(_len_alpha, -1)
@@ -34,6 +46,16 @@ def check_loss_grad_hess(
 def check_loss_eval(
     y_pred: np.ndarray, dtrain: lgb.basic.Dataset, alphas: List[float]
 ) -> Tuple[str, np.ndarray, bool]:
+    """
+    Return composite quantile loss
+    Args:
+        y_pred (np.ndarray) 
+        dtrain (lgb.basic.Dataset) 
+        alphas (List[float]) 
+
+    Returns:
+        Tuple[str, np.ndarray, bool]
+    """
     _len_alpha = len(alphas)
     _y_train = dtrain.get_label()
     _y_pred_reshaped = y_pred.reshape(_len_alpha, -1)
