23da4882e454c6ae1034db4fb424f03cc2f21ca6
RektPunk <rektpunk@gmail.com>
2024-06-30 16:57:33 +0900

    feat - update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="quantile-tree",
-    version="0.0.2",
+    version="0.0.3",
     packages=find_packages(),
     install_requires=[
         "numpy>=2.0.0",
