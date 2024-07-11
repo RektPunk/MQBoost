3c268a08af4388b7f93cad4545fa73fc9bd4710e
RektPunk <rektpunk@gmail.com>
2024-07-11 13:22:04 +0900

    release v0.1.2

--- quantile_tree/__init__.py
+++ quantile_tree/__init__.py
@@ -1,3 +1,3 @@
 from .model import QuantileRegressorLgb, QuantileRegressorXgb
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="quantile-tree",
-    version="0.1.1",
+    version="0.1.2",
     packages=find_packages(),
     install_requires=[
         "numpy>=2.0.0",
