cbceeb590cc7b6f2012534100cce8ccbd87ca746
RektPunk <rektpunk@gmail.com>
2024-09-07 17:28:38 +0900

    fix reindex and add params validate

--- mqboost/utils.py
+++ mqboost/utils.py
@@ -4,7 +4,14 @@ from itertools import chain, repeat
 import numpy as np
 import pandas as pd
 
-from mqboost.base import AlphaLike, ValidationException, XdataLike, YdataLike
+from mqboost.base import (
+    AlphaLike,
+    MQStr,
+    ParamsLike,
+    ValidationException,
+    XdataLike,
+    YdataLike,
+)
 
 
 def alpha_validate(
@@ -49,7 +56,7 @@ def prepare_x(
     _alpha_repeat_count_list = [list(repeat(alpha, len(x))) for alpha in alphas]
     _alpha_repeat_list = list(chain.from_iterable(_alpha_repeat_count_list))
 
-    _repeated_x = pd.concat([x] * len(alphas), axis=0)
+    _repeated_x = pd.concat([x] * len(alphas), axis=0).reset_index(drop=True)
     _repeated_x = _repeated_x.assign(
         _tau=_alpha_repeat_list,
     )
@@ -78,3 +85,11 @@ def delta_validate(delta: float) -> float:
         warnings.warn("Delta should be 0.05 or less.")
 
     return delta
+
+
+def params_validate(params: ParamsLike) -> None:
+    """Validates the model parameter ensuring its key dosen't contain 'objective'."""
+    if MQStr.obj.value in params:
+        raise ValidationException(
+            "The parameter named 'objective' must be excluded in params"
+        )
