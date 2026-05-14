4e217424eaa0107438a2e33abad0d3c18a4a8d49
unknown <rektpunk@gmail.com>
2026-05-14 16:51:53 +0900

    test action

--- mqboost/base.py
+++ mqboost/base.py
@@ -38,12 +38,10 @@ FUNC_TYPE: dict[ModelName, dict[TypeName, Any]] = {
 
 # Exception
 class FittingException(Exception):
-    """Raised when an operation requiring a fitted model is called on an unfitted model."""
-
+    # Raised when an operation requiring a fitted model is called on an unfitted model.
     pass
 
 
 class ValidationException(Exception):
-    """Raised when input parameters or data fail validation checks."""
-
+    # Raised when input parameters or data fail validation checks.
     pass
